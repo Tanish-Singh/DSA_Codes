12006d68cfa5ac919eedffcf4996ea136e65cbc4
Tanish Singh <tanish001singh@gmail.com>
2025-03-06 20:11:26 +0530

    Till Arrays

---
+++ Basics/13thArrays.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]; // decalred an empty array
+    int arr1[]={1,2,3};// declared an non empty array with dynamic size but this can't be empty
+    // 
+    cout<<"size of arr1 "<<sizeof(arr1)<<endl; 
+}
