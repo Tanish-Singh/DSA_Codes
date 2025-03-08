2d684b23cd41ceb530fe861fc134d4eae6f048ce
Tanish Singh <tanish001singh@gmail.com>
2025-03-08 15:24:53 +0530

    Till Vectors

--- Basics/13thArrays.cpp
+++ Basics/13thArrays.cpp
@@ -1,9 +1,76 @@
 #include <iostream>
 using namespace std;
 
+void changeArr(int arr[], int n){
+    for(int i=0; i<n;i++){
+        arr[i]=arr[i]*2;
+    }
+}
+
 int main(){
-    int arr[5]; // decalred an empty array
-    int arr1[]={1,2,3};// declared an non empty array with dynamic size but this can't be empty
-    // 
-    cout<<"size of arr1 "<<sizeof(arr1)<<endl; 
+    // int arr[5]; // decalred an empty array
+    // int arr1[]={1,2,3};// declared an non empty array with dynamic size but this can't be empty
+    // // 
+    // cout<<"size of arr1 "<<sizeof(arr1)<<endl; 
+
+    // // to read elements of array 
+    // for(int i=0; i<sizeof(arr1)/sizeof(int);i++){
+    //     cout<<arr1[i];
+    // }
+
+    // cout<<"Custom input in a array\n";
+    // int n;
+    // int a[5];
+    // for(int i=0; i<sizeof(a)/sizeof(int);i++){
+    //     cout<<"Enter the element no "<<i<<" :";
+    //     cin>>n;
+    //     a[i]=n;
+    // }
+
+    // Largest and smallest number
+    int lar = INT_MIN;
+    int sml=INT_MAX;
+    int arr2[10]={10,4,3,65,24,7,34,88,23,-4};
+    for(int i=0; i<sizeof(arr2)/sizeof(int);i++){
+        if(lar<arr2[i]){
+            lar=arr2[i];
+        }
+        else if(sml>arr2[i]){
+            sml=arr2[i]; // sml = min(nums[i], sml); this also works
+        }
+    }
+    cout<<"Maximum element is : "<<lar<<", Minimum element is : "<<sml;
+    
+    // pass by reference - means we pass the address of the main datatype, this will lead to changes within
+    // original stored values for example in arrays the reference is passed not values like vars
+    
+    int arrr[4]={3,41,3,5};
+    //cout<<changeArr(arrr,4); // this doesnt work cause we cant print array directly in cpp
+    changeArr(arrr, 4); // Call function to modify array
+
+    // Print the modified array
+    cout << "Modified array: ";
+    for (int i = 0; i < 4; i++) {
+        cout << arrr[i] << " ";
+    }
+    cout << endl;
+
+    // reverse an error
+    cout<<"Reversing Array\n";
+    int arrrr[7]={4,1,2,5,8,3,9};
+    int st=0;
+    int ls = sizeof(arrrr)/sizeof(int)-1;
+    int mid=0;
+    while(st<ls){
+        mid=arrrr[ls];
+        arrrr[ls]=arrrr[st];
+        arrrr[st]=mid;
+        st++;
+        ls--;
+    }
+    for (int h = 0; h < 7; h++) {
+        cout << arrrr[h] << " ";
+    }
+    cout << endl;
+
 }
---
+++ Basics/15thVectors.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector> //#include <bits/c++.h> can be used but this is orgs can create namespace erros
+// although not an error but better to use particular 
+using namespace std;
+int main(){
+    vector<int> vec; // empty vector
+
+    vector<int> vec1 = {1,2,3}; // with element
+    cout<<vec1[0]<<endl;
+
+    vector <int> vec2(5,0); // first is size of vector and after that is value at every index
+
+    cout<<vec2[0];
+    cout<<vec2[1];
+    cout<<vec2[2];
+    cout<<vec2[3];
+    cout<<vec2[4]<<endl;
+
+    // special loop for vectors is for each loop
+    for(int i: vec1){
+        cout<<i<<endl;
+    }
+
+    cout<<"size : "<<vec1.size()<<endl;
+
+    // to push 
+    vec1.push_back(8);
+    cout<<"Push element at last : "<< vec1[3]<<endl;
+
+    // pop back 
+    vec1.pop_back();// to remove value from last
+
+    // to see element at first and last and at a particular index
+    cout<<"Front of vetor "<<vec1.front()<<endl;
+    cout<<"Front of vetor "<<vec1.back()<<endl;
+    cout<<"Front of vetor "<<vec1.at(1)<<endl;
+
+    return 0;
+}
